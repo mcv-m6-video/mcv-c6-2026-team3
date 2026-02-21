56716bcb3255fbdb5a560ce659bea58ee0803795
Aleixrei73 <aleix.armero73@gmail.com>
2026-02-21 01:29:29 +0100

    Added preprocess abstraction to detector and combined bbox and mask video on one loop. Se puede saber pq lo has puesto separado?

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ import numpy as np
 from models import GaussianModel
 from detectors import CCDetector
 from utils import save_detections_txt
+import argparse
 
-VIDEO_PATH = "data/AICity_data/train/S03/c010/vdo.avi"
-RESULTS_PATH = "results/gaussian_k_8_cc_100"
+parse = argparse.ArgumentParser()
+parse.add_argument("-d", "--data", help="Data folder containing AICity_data", default="data/", type=str)
+parse.add_argument("-r", "--results", help="Folder to leave the results", default="results", type=str)
+parse.add_argument("-k", "--k", help="Deviation multiplier",default=2.5, type=float)
+parse.add_argument("-m", "--min", help="Minimum ammount of pixels for connected component", default=100, type=int)
+arg = parse.parse_args()
+
+
+K = arg.k
+MIN_CC_PIXELS = arg.min
+VIDEO_PATH = f"{arg.data}/AICity_data/train/S03/c010/vdo.avi"
+RESULTS_PATH = f"{arg.results}/gaussian_k_{K}_cc_{MIN_CC_PIXELS}"
 BG_PERCENTAGE = 0.25
-MIN_CC_PIXELS = 100
-K=8
-
-cap = cv.VideoCapture(VIDEO_PATH)
 
 os.makedirs(RESULTS_PATH, exist_ok=True)
+
+cap = cv.VideoCapture(VIDEO_PATH)
 
 frame_size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
 
@@ -24,12 +33,16 @@ mask_out = cv.VideoWriter(os.path.join(RESULTS_PATH, "mask.avi"),
                           frame_size,
                           isColor=False
                           )
+bbox_out = cv.VideoWriter(os.path.join(RESULTS_PATH, "detections.avi"), 
+                          cv.VideoWriter_fourcc(*'XVID'), 
+                          cap.get(cv.CAP_PROP_FPS), 
+                          frame_size,
+                          isColor=True)
 
 total_frame_num = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 bg_frame_num = int(BG_PERCENTAGE * total_frame_num)
 processed_frames = 0
 bg_frames = []
-masks = []
 
 model = GaussianModel(image_size=frame_size, K=K)
 detector = CCDetector(min_pixels=MIN_CC_PIXELS)
@@ -53,48 +66,21 @@ while True:
     
     mask = model(grey_frame).astype(np.uint8) * 255
     
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (4,4))
-    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (10,10))
-    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
-    
-    mask_out.write(mask)
-    masks.append(mask)
-    
     frame_id = processed_frames - bg_frame_num
-    detector.detect(mask, frame_id)
+    
+    bboxes, detection = detector.detect(mask, frame_id, preprocess=True) 
+    
+    mask_out.write(detection)
+    
+    for x, y, w, h in bboxes:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    bbox_out.write(frame)
+    
     processed_frames += 1
 
 cap.release()
 mask_out.release()
+bbox_out.release()
 
 print("WARNING: THE FRAME IDs START FROM 0 IN THE DETECTIONS, NOT FROM THE ORIGINAL VIDEO")
 save_detections_txt(detector.detections, os.path.join(RESULTS_PATH, "detections.txt"))
-
-cap = cv.VideoCapture(VIDEO_PATH)
-bbox_out = cv.VideoWriter(os.path.join(RESULTS_PATH, "detections.avi"), 
-                          cv.VideoWriter_fourcc(*'XVID'), 
-                          cap.get(cv.CAP_PROP_FPS), 
-                          frame_size,
-                          isColor=True)
-
-frame_count = 0
-while True:
-    ret, frame = cap.read()    
-    if not ret:
-        break
-    
-    if frame_count < bg_frame_num:
-        frame_count += 1
-        continue
-    
-    frame_id = frame_count - bg_frame_num
-    if frame_id in detector.detections:
-        for x, y, w, h in detector.detections[frame_id]:
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    bbox_out.write(frame)
-    frame_count += 1
-
-cap.release()
-bbox_out.release()
