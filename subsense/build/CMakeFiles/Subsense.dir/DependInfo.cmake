b9c885be8bb8ab987e171e665ed218d172b59d9a
adrrp <adriarp03@gmail.com>
2026-02-24 19:36:23 +0100

    subsense installed

---
+++ subsense/build/CMakeFiles/Subsense.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adria/Desktop/PC/MCV/C6/mcv-c6-2026-team3/subsense/Subsense/API.cpp" "CMakeFiles/Subsense.dir/Subsense/API.cpp.o" "gcc" "CMakeFiles/Subsense.dir/Subsense/API.cpp.o.d"
+  "/home/adria/Desktop/PC/MCV/C6/mcv-c6-2026-team3/subsense/Subsense/BackgroundSubtractorLBSP.cpp" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorLBSP.cpp.o" "gcc" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorLBSP.cpp.o.d"
+  "/home/adria/Desktop/PC/MCV/C6/mcv-c6-2026-team3/subsense/Subsense/BackgroundSubtractorLOBSTER.cpp" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorLOBSTER.cpp.o" "gcc" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorLOBSTER.cpp.o.d"
+  "/home/adria/Desktop/PC/MCV/C6/mcv-c6-2026-team3/subsense/Subsense/BackgroundSubtractorSuBSENSE.cpp" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorSuBSENSE.cpp.o" "gcc" "CMakeFiles/Subsense.dir/Subsense/BackgroundSubtractorSuBSENSE.cpp.o.d"
+  "/home/adria/Desktop/PC/MCV/C6/mcv-c6-2026-team3/subsense/Subsense/LBSP.cpp" "CMakeFiles/Subsense.dir/Subsense/LBSP.cpp.o" "gcc" "CMakeFiles/Subsense.dir/Subsense/LBSP.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ subsense/build/CMakeFiles/Subsense.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Subsense.
+# This may be replaced when dependencies are built.
